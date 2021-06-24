b5b353e09ad99548a5319c072343674ace2f6191
amira <amiraabouhadid@gmail.com>
2021-06-24 13:11:15 +0200

    update to include webpack --watch and serve --open

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,11 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
   devtool: 'inline-source-map',
+  devServer: {
+   contentBase: './dist',
+ },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
