03b72bcdf950f7b4cb3d695b91ec713e225eea1a
Shaher-11 <shahershamroukh@gmail.com>
2021-07-06 17:08:14 +0200

    Add extra test for the task

--- src/tests/task.test.js
+++ src/tests/task.test.js
@@ -25,3 +25,24 @@ test('id getter returns the task id', () => {
 test('dueDate getter returns the task dueDate', () => {
   expect(task.dueDate).toBe('2021-1-19');
 });
+test('priority getter returns the task priority', () => {
+  expect(task.priority).toBe('low');
+});
+
+test('status getter returns the task status', () => {
+  expect(task.status).toBe('To-do');
+});
+
+test('formatedDueDate returns a formated the task dueDate', () => {
+  expect(task.formatedDueDate() instanceof Date).toBeTruthy();
+});
+
+test('Title setter modifies task title', () => {
+  task.title = 'modified title';
+  expect(task.title).toBe('modified title');
+});
+
+test('description setter modifies the task description', () => {
+  task.description = 'modified description';
+  expect(task.description).toBe('modified description');
+});
