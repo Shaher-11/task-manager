c02e9e3eff572ec985dbe0c8e623af8e16f5e664
Shaher-11 <shahershamroukh@gmail.com>
2021-07-07 12:12:12 +0200

    Finish projce tests

--- src/tests/project.test.js
+++ src/tests/project.test.js
@@ -14,3 +14,28 @@ test('Title getter returns the project title', () => {
 test('Id getter returns the project id', () => {
   expect(project.id).toBe(1);
 });
+
+test('taskCounter getter returns the project task counter', () => {
+  expect(project.taskCounter).toBe(0);
+});
+
+test('Tasks getter returns the project tasks', () => {
+  expect(project.tasks instanceof Array).toBeTruthy();
+  expect(project.tasks.length).toBe(0);
+});
+
+test('Title setters modifies the project title', () => {
+  project.title = 'modified title';
+  expect(project.title).toBe('modified title');
+});
+
+test('addTask method adds task to the project', () => {
+  project.addTask(task);
+  expect(project.tasks.length).toBe(1);
+  expect(project.taskCounter).toBe(1);
+});
+
+test('removeTask method removes task from the project', () => {
+  project.removeTask(task);
+  expect(project.tasks.length).toBe(0);
+});
