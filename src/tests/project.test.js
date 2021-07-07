b1670f7d415d6ec1768602920ef8489545998c4d
Shaher-11 <shahershamroukh@gmail.com>
2021-07-07 10:36:51 +0200

    Add first tests for project

--- src/tests/project.test.js
+++ src/tests/project.test.js
@@ -3,3 +3,14 @@ const Task = require('../task').default;
 
 const project = new Project('project test', 1);
 const task = new Task('task test', 'task description', '2021-1-19', 'low', 1, 1);
+test('Create new project', () => {
+  expect(project instanceof Project).toBeTruthy();
+});
+
+test('Title getter returns the project title', () => {
+  expect(project.title).toBe('project test');
+});
+
+test('Id getter returns the project id', () => {
+  expect(project.id).toBe(1);
+});
