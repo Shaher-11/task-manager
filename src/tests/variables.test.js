6824366f7b620b0cb64b69fe496d1e4aac90330a
Shaher-11 <shahershamroukh@gmail.com>
2021-07-07 15:11:09 +0200

    Add the variables tests

--- src/tests/variables.test.js
+++ src/tests/variables.test.js
@@ -14,3 +14,17 @@ const project1 = new Project('project1 test', 1);
 const project2 = new Project('project2 test', 2);
 const listProjects = [project1, project2];
 
+test('FindProject Returns index of a project in a list of projects', () => {
+  expect(findProject(listProjects, 2)).toBe(1);
+});
+
+test('deleteFromProjectList modifies project list by removing project with index', () => {
+  deleteFromProjectList(listProjects, 1);
+  expect(listProjects.length).toBe(1);
+});
+
+test('addTaskToProject modify a project from list of project by adding a task to it', () => {
+  addTaskToProject(listProjects, 2, task);
+  expect(listProjects[0].tasks.length).toBe(1);
+});
+
